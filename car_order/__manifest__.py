ef41ee3572c81ef2fece47647464324611075d52
Pichchanok Nuchpan <pichchanok.n@gmail.com>
2023-02-19 22:12:25 +0700

    car_order v0.8

---
+++ car_order/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "Car Order",
+    'summary': """
+        Car OrderModule 
+    """,
+    'description': """""",
+    'author': "Around Enterprise Consulting Co., Ltd.",
+    'website': "https://www.around.co.th",
+    "version": "15.0.0.8.0",
+    "depends": [
+        'base',
+        'product',
+        'account',
+        'sale',
+        'fleet_product_link',
+    ],
+    "data": [
+        "data/ir_sequence_data.xml",
+        "security/ir.model.access.csv",
+        "views/car_order.xml",
+    ]
+}
