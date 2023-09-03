cdc25a662a7241733b676b663e1b36da65fe2a22
Pichchanok Nuchpan <pichchanok.n@gmail.com>
2023-09-03 14:12:15 +0700

    fix condition check access right

--- car_order/models/res_partner.py
+++ car_order/models/res_partner.py
@@ -20,3 +20,11 @@ class Partner(models.Model):
             if self.env.user.group_sale_salesman() and crm_team_id:
                 domain.insert(0, ['team_id', '=', crm_team_id.id])
         return super().search_read(domain=domain, fields=fields, offset=offset, limit=limit, order=order)
+
+    @api.model
+    def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
+        if self._context.get('filter_group_sale_salesman'):
+            crm_team_id = self.env.user.crm_team_id
+            if self.env.user.group_sale_salesman() and crm_team_id:
+                domain.insert(0, ['team_id', '=', crm_team_id.id])
+        return super().read_group(domain, fields, groupby, offset, limit, orderby, lazy)
