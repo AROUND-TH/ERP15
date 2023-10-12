ff9d0d87b72f1d07f15fcd9287bed5400d94313f
Pichchanok Nuchpan <pichchanok.n@gmail.com>
2023-10-13 00:33:50 +0700

    BA-389: purchase custom mail massage when confirm po

---
+++ custom_purchase/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Customize: Purchase",
+    'summary': """
+        Customize of Purchase, Purchase Stock Module
+    """,
+    'description': """""",
+    'author': "Around Enterprise Consulting Co., Ltd.",
+    'website': "https://www.around.co.th",
+    "version": "15.0.1.0.0",
+    "depends": [
+        'base',
+        'stock',
+        'purchase',
+        'purchase_stock'
+    ],
+    "data": [
+        "data/mail_templates.xml",
+    ]
+}
